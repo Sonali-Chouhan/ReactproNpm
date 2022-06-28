e6a8c4182d479ba25970c17e8f1efb817084994d
Sonali-Chouhan <sonaliyuvasoft363@gmail.com>
2022-06-28 16:41:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
+echo "# ReactproNpm" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Sonali-Chouhan/ReactproNpm.git
+git push -u origin main
